c5ea67cc4f0760def405f282744c56b7b92f5868
marcus-hugo <78324205+marcus-hugo@users.noreply.github.com>
2022-04-08 10:15:55 -0500

    added remove functionality

--- app.js
+++ app.js
@@ -22,26 +22,66 @@ for (let btn of addBtns) {
 function renderItems(services) {
     let listItems = '';
     let totalItems = '';
+
     for (let i = 0; i < services.length; i++) {
         listItems += `
             <li class="rendered-items">
-                <p>${servicesArr[i].description.slice(0, servicesArr[i].description.length - 3)}</p>
-                <p>${servicesArr[i].amount.replace('', '$')}</p>
+                <p>${services[i].description.slice(0, servicesArr[i].description.length - 3)}</p>
+                <button type="button" id='${services[i].amount}' class="remove-button">Remove</button>
+                <p>${services[i].amount.replace('', '$')}</p>
             </li>
         `;
     }
-    if (servicesArr.length === 1) {
-        totalItems = parseInt(servicesArr[0].amount);
-    } else if (servicesArr.length === 2) {
-        totalItems = parseInt(servicesArr[0].amount) + parseInt(servicesArr[1].amount);
-    } else if (servicesArr.length === 3) {
-        totalItems = parseInt(servicesArr[0].amount) + parseInt(servicesArr[1].amount) + parseInt(servicesArr[2].amount);
+
+    if (services.length === 1) {
+        totalItems = parseInt(services[0].amount);
+    } else if (services.length === 2) {
+        totalItems = parseInt(services[0].amount) + parseInt(services[1].amount);
+    } else if (services.length === 3) {
+        totalItems = parseInt(services[0].amount) + parseInt(services[1].amount) + parseInt(services[2].amount);
     }
+
     ul.innerHTML = listItems;
     grandTotal.innerText = totalItems.toString().replace('', '$');
 
-    if (servicesArr.length >=1 && servicesArr.length <= 3) {
+    if (services.length >=1 && services.length <= 3) {
         submitBtn.classList.remove('disabled');
         submitBtn.disabled = false;
     }
+
+    let removeBtns = document.querySelectorAll('.remove-button');
+
+    for (let btn of removeBtns) {
+        btn.addEventListener('click', function() {
+           let findService = (service) => service.amount === btn.id;
+           let serviceIndex = servicesArr.findIndex(findService);
+            console.log(serviceIndex);
+            servicesArr.splice(serviceIndex, 1);
+
+            renderItems(servicesArr);
+
+            if(servicesArr.length === 0) {
+                submitBtn.classList.add('disabled');
+                submitBtn.disabled = true;
+
+               for(let btn of addBtns) {
+                   btn.classList.remove('disabled')
+                   btn.disabled = false;
+               }
+            }
+          
+        })
+    }
+    
 }
+
+submitBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    
+    servicesArr = []
+    renderItems(servicesArr);
+    for(let btn of addBtns) {
+        btn.classList.remove('disabled');
+        btn.disabled = false;
+    }
+})
